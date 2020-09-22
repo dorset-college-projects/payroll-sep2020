c7496c18e9f3534c8d59a17fdcfc66c31ca97c19
John Rowley <outsidethebox@eircom.net>
2020-09-22 15:58:15 +0100

    Added earnings to one file as example

--- src/main/java/payroll/Main.java
+++ src/main/java/payroll/Main.java
@@ -30,6 +30,9 @@ public class Main {
         System.out.println(he.toString());
         System.out.println(ce.toString());
         System.out.println(bpce.toString());
+        
+        // Output the earnings 
+        System.out.println(bpce.Earnings());
 
     }
 
